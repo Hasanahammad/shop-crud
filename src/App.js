43402da3cb76c2c3c294f56855b725fce522c2a4
Hasan Ahammad <33357004+Hasanahammad@users.noreply.github.com>
2024-01-24 12:18:54 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path='/product/edit/:productid' element={<EditProduct />}></Route>
         </Routes>
       </BrowserRouter>
+    <Apicall />
     </div>
   );
 
