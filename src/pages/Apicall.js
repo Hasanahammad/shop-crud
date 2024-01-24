765c6a036b3b78841f1d59b7ebb30b4b55fdc1af
Hasan Ahammad <33357004+Hasanahammad@users.noreply.github.com>
2024-01-24 12:18:02 +0600

    Update Apicall.js

--- src/pages/Apicall.js
+++ src/pages/Apicall.js
@@ -1,1 +1,40 @@
+import React, { useEffect, useState } from 'react';
 
+const Apicall = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://45.64.132.149:3388/zab/mmphonenumber?PhoneNumber=01795254586');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data && (
+        <div>
+          {/* Render data here */}
+          {JSON.stringify(data)}
+          console.log(JSON.stringify(data));
+        </div>
+      )}
+      {error && (
+        <div>
+          {/* Render error message here */}
+          {JSON.stringify(error)}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Apicall;
